cdcfe215ed5e8818ddc851b1d4e275a16b188c21
nick-hu <nicholas.squishy@gmail.com>
2013-05-18 23:05:49 -0700

    added axes

--- graph/graphimg.py
+++ graph/graphimg.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+from math import *
 import Image
 from func import *
 
@@ -14,10 +15,11 @@ def pixel(x, y):
     ypix = round(center[1] + ystep * y)
     return (int(xpix), int(opt['height'] - ypix))
 
-# Graph options
+# Graph options (defaults)
 opt = {'xmin': -20, 'xmax': 20, 'ymin': -20, 'ymax': 20,
-       'xscl': 1, 'yscl': 1, 'width': 400, 'height': 400,
-       'axes': 3, 'point': 2, 'backg': 'white'}
+       'width': 1000, 'height': 1000, 'xscl': 1, 'yscl': 1,
+       'axes': 2, 'drawaxes': True, 'axescolor': (0, 0, 0),
+       'point': 2, 'backg': (255, 255, 255)}
 
 while True:
     filename = raw_input('Write graph to file: ')
@@ -32,6 +34,35 @@ while True:
     graph = Image.new('RGB', (opt['width'], opt['height']), opt['backg'])
     gpix = graph.load()  # Create image and pixel access object
 
+    if opt['drawaxes']:
+        xcent, ycent, ticks = opt['width'] // 2, opt['height'] // 2, []
+        for xtick in flrange(opt['xmin'], opt['xmax'] + 1, opt['xscl']):
+            ticks.append(pixel(xtick, 0))
+        for ytick in flrange(opt['ymin'], opt['ymax'] + 1, opt['yscl']):
+            ticks.append(pixel(0, ytick))
+        print ticks
+
+        for l in xrange(ycent - opt['axes'] + 1, ycent + opt['axes']):
+            for w in xrange(opt['width']):
+                gpix[w, l] = opt['axescolor']
+                if (w, l) in ticks:
+                    for tw in xrange(w-1, w+2):
+                        if 0 <= tw < opt['width']:
+                            gpix[tw, l - opt['axes']] = opt['axescolor']
+                            gpix[tw, l - opt['axes'] - 1] = opt['axescolor']
+                            gpix[tw, l - opt['axes'] - 2] = opt['axescolor']
+        for l in xrange(xcent - opt['axes'] + 1, xcent + opt['axes']):
+            for h in xrange(opt['height']):
+                gpix[l, h] = opt['axescolor']
+                if (l, h) in ticks:
+                    for th in xrange(h-1, h+2):
+                        if 0 <= th < opt['height']:
+                            gpix[l + opt['axes'], th] = opt['axescolor']
+                            gpix[l + opt['axes'] + 1, th] = opt['axescolor']
+                            gpix[l + opt['axes'] + 2, th] = opt['axescolor']
+
+    graph.save(filename)
+
     while True:
         name = raw_input('\nFunction name: ')
         if name == '':
@@ -41,25 +72,31 @@ while True:
         exec name + ' = ' + fn
         exec 'colour = ' + colour
 
+        # Decimal equivalent of one pixel
         pix = float(opt['xmax']-opt['xmin']) / float(opt['width'])
 
+        # Iterate through all possible x values
         for val in flrange(opt['xmin'], opt['xmax'] + pix, pix):
             exec 'yval = ' + name + '.val(' + str(val) + ')'
 
             coords, point = pixel(val, yval), []
+            # Thicken point by surrounding with a square
             if coords[1] is not None:
                 pl, pr = coords[0] - opt['point'] + 1, coords[0] + opt['point']
                 pt, pb = coords[1] - opt['point'] + 1, coords[1] + opt['point']
                 point.append(coords)
-                for xc in xrange(pl, pr):
-                    for yc in xrange(pt, pb):
-                        point.append((xc, yc))
+                try:
+                    for xc in xrange(pl, pr):
+                        for yc in xrange(pt, pb):
+                            point.append((xc, yc))
+                except OverflowError:
+                    pass
 
-            for c in point:
-                validx = 0 <= c[0] < opt['width']
-                validy = 0 <= c[1] < opt['height']
+            for coord in point:
+                validx = 0 <= coord[0] < opt['width']
+                validy = 0 <= coord[1] < opt['height']
                 if validx and validy and coords[1] is not None:
-                    gpix[c] = colour
+                    gpix[coord] = colour
 
         graph.save(filename)
 
